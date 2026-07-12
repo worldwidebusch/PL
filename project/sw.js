4cfa256b3a722869fffba15805218dee1c27ec59
worldwidebusch <193455738+worldwidebusch@users.noreply.github.com>
2026-07-11 20:53:55 -0600

    Build complete freelancer registration form

--- project/sw.js
+++ project/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION = "v4-account-menu";
+const CACHE_VERSION = "v5-registration-flow";
 const CACHE_PREFIX = "prolinker-";
 const PRECACHE = `${CACHE_PREFIX}precache-${CACHE_VERSION}`;
 const PAGE_CACHE = `${CACHE_PREFIX}pages-${CACHE_VERSION}`;
